2482a454b213cd7ef6d66fd935b049b92b647d7f
Hafeez Syed <feeziman007@hotmail.com>
2018-12-02 13:54:42 +1100

    css and fonts files updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-//alert('hey');
+import React from "react";
+import { render } from "react-dom";
+
+import Router from "./components/Router";
+import "./css/style.css";
+
+render(<Router />, document.querySelector("#main"));
